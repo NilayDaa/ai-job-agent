423bbd4e4c7125721d698ace3fdc141be90b2dd9
Nilay Das <ag3567@student.jamk.fi>
2026-07-10 03:17:59 +0300

    deployment edits

--- app/main.py
+++ app/main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.core.database import init_db
+
+
+# IMPORTANT:
+# Create database tables before importing routers
+init_db()
+
+
 from app.api.jobs import router as jobs_router
 from app.api import cv
 from app.api import auth
-from fastapi.middleware.cors import CORSMiddleware
-
 
 
 app = FastAPI()
+
 
 app.include_router(cv.router)
 
@@ -14,10 +23,12 @@ app.include_router(auth.router)
 
 app.include_router(jobs_router)
 
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "http://localhost:5173",
+        # add your Vercel URL later
     ],
     allow_credentials=True,
     allow_methods=["*"],
@@ -27,4 +38,6 @@ app.add_middleware(
 
 @app.get("/")
 def home():
-    return {"message": "AI Job Agent running"}
+    return {
+        "message": "AI Job Agent running"
+    }
